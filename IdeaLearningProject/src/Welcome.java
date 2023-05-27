d5e96d37747896eb70efbeb0fc21872b81165ca0
Ram Kadambi <ram.kadambi@gmail.com>
2023-05-27 17:42:34 +0530

    Initial Commit

--- IdeaLearningProject/src/Welcome.java
+++ IdeaLearningProject/src/Welcome.java
@@ -3,8 +3,8 @@ import java.util.List;
 
 class Welcome {
     public static void main(String[] args) {
-        int[] array = {5, 6, 7, 8};
-        System.out.printf("average of array %s is %s%n", Arrays.toString(array), findAverage(array));
+        int[] array = {5, 6, 7, 8, 10};
+        System.out.printf("The new average of array %s is %s%n", Arrays.toString(array), findAverage(array));
     }
 
     private static double findAverage(int[] values) {
